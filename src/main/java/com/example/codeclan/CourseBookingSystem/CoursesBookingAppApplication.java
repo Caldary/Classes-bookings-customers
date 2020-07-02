622173360e64f5ecf9397a292d04e6361438eced
Paulo Rocha <paulorocha1988@gmail.com>
2020-07-02 10:08:10 +0100

    initialized

---
+++ src/main/java/com/example/codeclan/CourseBookingSystem/CoursesBookingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.CourseBookingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesBookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesBookingAppApplication.class, args);
+	}
+
+}
